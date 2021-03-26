3925b64a0e6d20682da48b9dd7b265c992cdde26
Giero <giero.spam@gmail.com>
2021-03-26 11:34:11 +0100

    Initialized project

---
+++ src/main/java/com/giero/sma/SimpleMySqlApplication.java
@@ -0,0 +1,13 @@
+package com.giero.sma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleMySqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleMySqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/giero/sma/SimpleMySqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giero.sma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleMySqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
